3400019bfb9ff5f99f2ba96ed77249e67632c72a
Nathanna <hello@nathannaofficial.com>
2021-01-01 01:42:39 -0500

    Platform Changes

---
+++ system-bu/Main.java
@@ -0,0 +1,7 @@
+package org.pipeboard.system;
+
+public class Main {
+    public Main() {
+        System.out.print("test123");
+    }
+}
---
+++ system-bu/Run.java
@@ -0,0 +1,9 @@
+package org.pipeboard.system;
+
+import org.pipeboard.system.Main;
+
+public class Run {
+    public static void main(String[] args) {
+        Main m1 = new org.pipeboard.system.Main();
+    }
+}
