48c1a0616ad62e6aa2bac65111d497e7f49656fe
Nathanna <hello@nathannaofficial.com>
2021-01-04 16:56:02 -0500

    Module Meathod Changes

--- installers/moduler.sh
+++ installers/moduler.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# --START_CUSTOM--
 docker pull ubuntu
 docker pull httpd
 docker pull mysql
+#--END_CUSTOM--
+
+printf "\n\nFinished installing modules, Pipeboard is all setup. :)\n\n";
