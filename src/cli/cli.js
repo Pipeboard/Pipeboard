3e1d865191adce9f57e567bc9169f8571a61467b
Nathanna <hello@nathannaofficial.com>
2021-01-05 14:31:15 -0500

    CLI Changes

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,3 +1,17 @@
+import arg from 'arg';
+
 export function cli(args) {
     console.log("Pipeboard CLI test.");
+
+    args = args.slice(2, args.length);
+
+    console.log(args);
+
+    if(args[0] == "prep") {
+        require('./prep.js');
+    } else if(args[0] == "run") {
+        require('../system/main.js');
+    } else if(args[0] == "update") {
+        require('./update.js');
+    }
 }
