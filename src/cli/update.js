c226fed232f12a01cf1d36e8fa1ca14158a587cc
Nathanna <hello@nathannaofficial.com>
2021-01-05 17:39:44 -0500

    Update update.js

--- src/cli/update.js
+++ src/cli/update.js
@@ -224,7 +224,7 @@ xhttp.onreadystatechange = function() {
                 json.forEach(function(i) {
                     if(i.tag_name == "v" + ltag) {
                         currentversiondata = i;
-                        nextversiondatanum = whereinloop + 2;
+                        nextversiondatanum = whereinloop - 1;
                     }
 
                     if(whereinloop == nextversiondatanum) {
