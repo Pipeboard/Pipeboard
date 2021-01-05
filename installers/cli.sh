d9839ac15dcaf3f16614f6bbc7258c467751f871
Nathanna <hello@nathannaofficial.com>
2021-01-05 10:49:58 -0500

    Installers moved to PipeCLI

--- installers/cli.sh
+++
@@ -1,6 +0,0 @@
-function pipeboard() {
- if [ $1 = 'prep' ]
- then
-    bash ./installers/prep.sh
- fi
-}
--- installers/moduler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# --START_CUSTOM--
-docker pull ubuntu
-docker pull httpd
-docker pull mysql
-#--END_CUSTOM--
-
-printf "\n\nFinished installing modules, Pipeboard is all setup. :)\n\n";
