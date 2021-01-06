dbd0d4426758f3c40d566de45f5f7a750d3924e6
Nathanna <hello@nathannaofficial.com>
2021-01-06 16:44:06 -0500

    Update prep.js

--- src/linux/cli/prep.js
+++ src/linux/cli/prep.js
@@ -1,6 +1,7 @@
 import arg from 'arg';
 
 const ncmd = require('node-cmd');
+const fs = require('fs');
 
 let pmc = "apt-get";
 ncmd.run("apt-get --version &> /dev/null", function(err, data, stderr) {
@@ -15,7 +16,14 @@ ncmd.run("apt-get --version &> /dev/null", function(err, data, stderr) {
 
     ncmd.runSync("sudo " + pmc + " update | sudo " + pmc + " install apt-transport-https ca-certificates curl gnupg-agent software-properties-common");
     ncmd.runSync("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -");
-    ncmd.runSync("sudo " + pmc + " update | sudo add-apt-repository deb https://download.docker.com/linux/ubuntu stable' | sudo " + pmc + " install docker-ce docker-ce-cli containerd.io | npm install | sudo systemctl start docker | sudo service docker start | pipeboard apps pull --first-time | npm install")
+    ncmd.runSync("sudo " + pmc + " update | sudo add-apt-repository deb https://download.docker.com/linux/ubuntu stable' | sudo " + pmc + " install docker-ce docker-ce-cli containerd.io | npm install | sudo systemctl start docker | sudo service docker start | pipeboard apps pull --first-time | npm install");
+    ncmd.runSync("sudo systemctl enable docker | echo manual | sudo tee /etc/init/docker.override | sudo chkconfig docker on");
+
+    fs.readFile("/etc/docker/daemon.json", function(err, data) {
+        let dj = JSON.parse(data);
+        dj.hosts[0] = "unix://" + path.join(__dirname, "../") + "system/pb-docker-daemon.sock";
+        fs.writeFileSync("/etc/docker/daemon.json", JSON.stringify(dj));
+    });
 
     console.log("All done! Enjoy your linux Pipeboard installation! :)");
 });
