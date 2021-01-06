703348286820b331769c7082727026dad3691a0d
Nathanna <hello@nathannaofficial.com>
2021-01-06 12:59:27 -0500

    Update prep.js

--- src/cli/prep.js
+++ src/cli/prep.js
@@ -100,8 +100,9 @@ function setup_for_linux() {
 
         console.log("Downloading and installing dependencies and preperations, this could take a second...");
 
-        ncmd.runSync("sudo " + pmc + " update | sudo " + pmc + " update | sudo " + pmc + " install apt-transport-https ca-certificates curl gnupg-agent software-properties-common | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -");
-        ncmd.runSync("apt-get update | sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable' | sudo " + pmc + " install docker-ce docker-ce-cli containerd.io | npm install | sudo systemctl start docker | sudo service docker start | pipeboard apps pull --first-time | npm install")
+        ncmd.runSync("sudo " + pmc + " update | sudo " + pmc + " install apt-transport-https ca-certificates curl gnupg-agent software-properties-common");
+        ncmd.runSync("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -");
+        ncmd.runSync("sudo " + pmc + " update | sudo add-apt-repository deb https://download.docker.com/linux/ubuntu stable' | sudo " + pmc + " install docker-ce docker-ce-cli containerd.io | npm install | sudo systemctl start docker | sudo service docker start | pipeboard apps pull --first-time | npm install")
 
         console.log("All done! Enjoy your linux Pipeboard installation! :)");
     });
