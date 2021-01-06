c660ea258c1d23a6675a93c599d00a19f4e78157
Nathanna <hello@nathannaofficial.com>
2021-01-06 02:57:38 -0500

    New Prep Script

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -2,11 +2,11 @@ import arg from 'arg';
 
 let path = require('path');
 
-export function cli(args) {
-    args = args.slice(2, args.length);
+export function cli(args1) {
+    let args = args1.slice(2, args1.length);
 
     if(args[0] == "prep") {
-        require(path.join(__dirname, 'prep.js'));
+        require(path.join(__dirname, 'prep.js')).parse(args1);
     } else if(args[0] == "run") {
         require(path.join(__dirname, '../system/run.js'));
     } else if(args[0] == "update") {
