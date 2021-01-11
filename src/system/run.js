b47f451ab7b7267bdc2121eafefe208df5357c73
Nathanna <hello@nathannaofficial.com>
2021-01-11 12:22:49 -0500

    "Run2" Merge To "Run"

--- src/system/run.js
+++ src/system/run.js
@@ -1,58 +1,9 @@
-const Docker = require('dockerode');
+const compose = require("docker-compose");
+const compose_nc = require("docker-compose-nocompose");
+const path = require("path");
+const eventer = require("./eventer.js");
 
 require('./socket.js');
 require('./api.js');
 
-const docker = new Docker();
-
-var parentcontainer = docker.getContainer("pipeboard");
-var systemcontainer = null;
-var usercontainers = [];
-
-docker.listContainers(function(err, data) {
-  if(err) console.log(err);
-
-  let datax = JSON.parse(JSON.stringify(data));
-  
-  datax.forEach(function(data2) {
-    if (data2.Names.includes("pipeboard")) {
-      parentcontainer = docker.getContainer(data2.Id);
-    }
-  });
-});
-
-if(parentcontainer == null) {
-  docker.createContainer({Cmd: ['/bin/bash'], name: 'pipeboard'}, function(err, container) {
-    if (err) console.log(err);
-    parentcontainer = container;
-    ctnu(parentcontainer);
-  });
-} else {
-  ctnu(parentcontainer);
-}
-
-function ctnu(parentcontainer) {
-  var pbsgot = null; 
-
-  docker.getContainer("pipeboard_box_system", function(err, container) {
-    if(err) console.log(err);
-    pbsgot = container;
-
-    if(pbsgot !== null) {
-      ctnu(pbsgot);
-    } else {
-      docker.createContainer({Cmd: ['/bin/bash'], name: "pipeboard_box_system"}, function (err, container) {
-        if(err) { console.log(err); }
-        ctnu(container);
-      })
-    }
-  
-    function ctnu2() {
-      systemcontainer = pbsgot;
-      systemcontainer.start({Cmd: ['echo test'], function(err, data) {
-        if(err) { console.log(err); }
-        console.log(data);
-      }})
-    }
-  });
-}
+process.stdin.resume();
