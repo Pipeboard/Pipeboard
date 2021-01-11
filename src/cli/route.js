6b3627f0edbaf40ee9b5a5bbf95662324c49ad35
Nathanna <hello@nathannaofficial.com>
2021-01-11 12:15:59 -0500

    /src/platform Removed
    Moved just clis to per-platform.

---
+++ src/cli/route.js
@@ -0,0 +1,1 @@
+require(path.join(__dirname, "/" + platform))
--- src/router.js
+++
@@ -1,6 +0,0 @@
-let platform = process.platform;
-require(path.join(__dirname, platform + '/system/run.js')).cli(process.argv);
-
-function fromoldpath(path) {
-    require(path.join(__dirname, platform + path));
-}
